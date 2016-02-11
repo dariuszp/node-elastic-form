75fd0ef9a0be7783b600b771fb172a950da8272a
Dariusz Półtorak <dariusz.poltorak@happyendians.pl>
2016-02-11 14:54:57 +0100

    helper

---
+++ doc/test.js
@@ -0,0 +1,27 @@
+'use strict';
+
+//
+// This script print web page with all inputs
+//
+
+var express = require('express');
+var glob = require('glob');
+var app = express();
+
+app.get('/', function (req, res) {
+    var response = ['<h1>INPUTS</h1>'];
+    glob('/home/dariuszp/www/node-elastic-form/src/**/Input*.js', {}, function (err, files) {
+        if (err) {
+            throw err;
+        }
+        for (var i = 0; i < files.length; i++) {
+            response.push(`<h2>${files[i].split('/').pop()}</h2>`);
+            var Input = require(files[i]);
+            var inputObj = new Input('input' + i);
+            response.push(`<div style="padding: 50px; margin: 10px; border: 2px solid #ccc;">${inputObj.toString()}</div>`);
+        }
+        res.end(response.join('<br>'));
+    });
+});
+
+app.listen(3000);
